d328011a9221d59a17146517149317e109ecf328
abhyudaysoni <abhyudaysoni03@gmail.com>
2022-07-01 17:38:46 +0530

    nothing

---
+++ practice/app.js
@@ -0,0 +1,20 @@
+/*
+function sumMaxMin(a, b, c, d, e) {
+    // write code here
+    // return the output , do not use console.log here
+
+    let arr = [a, b, c, d, e];
+    arr.sort();
+    return arr[0] + arr[4];
+}
+
+console.log(sumMaxMin(4, 8, 6, -9, 6));
+
+
+function doesItContain(str1, str2) {
+    // write code here
+    // return the output , do not use console.log here
+    return str2.includes(str1);
+}
+console.log(doesItContain("abc", "abcdef"));
+*/
